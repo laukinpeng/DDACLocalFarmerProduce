5df31ffc6f116d37509d9eb5130b0546ad2d9cda
Lau Kin Peng <laukinpeng@gmail.com>
2021-03-17 18:32:40 +0800

    added scaffolded

---
+++ DDACLocalFarmerProduce/Data/DDACProductContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DDACLocalFarmerProduce.Models;
+
+namespace DDACLocalFarmerProduce.Data
+{
+    public class DDACProductContext : DbContext
+    {
+        public DDACProductContext (DbContextOptions<DDACProductContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DDACLocalFarmerProduce.Models.Product> Product { get; set; }
+    }
+}
---
+++ DDACLocalFarmerProduce/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DDACLocalFarmerProduce.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; } // PK for the table
+        public string ProductName { get; set; }
+        public DateTime ProductHarvestDate { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public string Description { get; set; }
+    }
+}
